5078e216c95ce2a7ad79621df7739623af2bd5e7
Han-Bayraktar <beyaztbayraktar@gmail.com>
2024-12-18 20:52:53 +0300

    Account controllerı oluşturularak bu controllera bağlı  login ve register sayfaları oluşturuldu

---
+++ WPOdev/WebProje/wwwroot/js/login-register.js
@@ -0,0 +1,19 @@
+﻿const showHiddenPass = (inputId, iconId) => {
+    const input = document.getElementById(inputId),
+        icon = document.getElementById(iconId);
+
+    icon.addEventListener('click', () => {
+        if (input.type === 'password') {
+            input.type = 'text';
+            icon.classList.add('ri-eye-line');
+            icon.classList.remove('ri-eye-off-line');
+        } else {
+            input.type = 'password';
+            icon.classList.remove('ri-eye-line');
+            icon.classList.add('ri-eye-off-line');
+        }
+    });
+};
+
+showHiddenPass('login-pass', 'login-eye');
+showHiddenPass('register-pass', 'register-eye');
