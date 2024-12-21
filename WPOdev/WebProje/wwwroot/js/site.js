00762a4ded14b1f0f49baf5cb0b519fc6afe5d34
Berat Yılmaz <126776491+Berbtv@users.noreply.github.com>
2024-12-21 23:39:15 +0300

    Hakkımızda ve iletişim sayfaları için gerekli frontend kodları yazıldı.

--- WPOdev/WebProje/wwwroot/js/site.js
+++ WPOdev/WebProje/wwwroot/js/site.js
@@ -1,4 +1,44 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+let scrollMargin = 0;
 
+if (window.innerWidth > 1600) {
+    scrollMargin = 10;
+} else if (window.innerWidth <= 900) {
+    scrollMargin = 50;
+} else {
+    scrollMargin = 0;
+}
+
+window.onresize = function (event) {
+    if (window.innerWidth > 1600) {
+        scrollMargin = 10;
+    } else if (window.innerWidth <= 900) {
+        scrollMargin = 50;
+    } else {
+        scrollMargin = 0;
+    }
+};
+
+$("#Home-Nav").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $("#Home").offset().top,
+        },
+        1000
+    );
+    return false;
+});
+
+
+
+$("#Contact-Nav").click(function () {
+    $("html,body ").animate(
+        {
+            scrollTop: $("#Contact").offset().top - scrollMargin,
+        },
+        1000
+    );
+    return false;
+});
 // Write your JavaScript code.
