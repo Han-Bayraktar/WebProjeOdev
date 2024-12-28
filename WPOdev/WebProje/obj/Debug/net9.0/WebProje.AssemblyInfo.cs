49c9efba4f2fcf32eb57d4114d32e6512b7e2ab8
Han-Bayraktar <beyaztbayraktar@gmail.com>
2024-12-28 22:18:02 +0300

    Bitti.

--- WPOdev/WebProje/obj/Debug/net9.0/WebProje.AssemblyInfo.cs
+++ WPOdev/WebProje/obj/Debug/net9.0/WebProje.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7a69d5059222e1be88b962a4a044955513aee25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97ceab4d4af228a40a2e3e827a626931ca536c3d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
